e99302149aa927e1adca107e1320bb055c7a3b00
Rosario Cecilio-Flores-Elie <rosariocfe@Rosarios-MacBook-Pro.local>
2022-08-24 11:37:21 -0400

    Fixed a few fucntions

--- functions.py
+++ functions.py
@@ -1,23 +1,27 @@
 # Here are some functions that you can fill in to make them work.
 # Replace the "raise NotImplementedError" line with the correct code!
-   
-def print_the_instructors_name():
     raise NotImplementedError
 
 def print_the_number_of_this_room():
-    raise NotImplementedError
-
+   return "Rosario Cecilio-Flores-Elie"
+	#raise NotImplementedError
 def split_this_string_at_each_space(input_string):
-    raise NotImplementedError
+   return input_string.split( "  ")
+
 
 def take_the_average_of_these_numbers(a, b):
-    raise NotImplementedError
+   #raise NotImplementedError
 
 def print_the_time_now_using_astropy():
-    raise NotImplementedError
+    #raise NotImplementedError
 
 def return_the_minimum_of_two_numbers(a, b):
-    raise NotImplementedError
+    #raise NotImplementedError
 
 def return_the_std_of_two_numbers(a, b):
-    raise NotImplementedError
+    #raise NotImplementedError
+
+def Hello world()
+    print ("Hello World")
+
+
